ef39c6692a47174e965bc4a36535c3c56e9c4e40
TricMan <p98tretyak@gmail.com>
2024-10-03 17:27:01 +0300

    update area

--- app/models/field.rb
+++ app/models/field.rb
@@ -1,5 +1,5 @@
 class Field < ApplicationRecord
-  after_save :calculate_area
+  before_save :calculate_area
 
   validates_presence_of :name, :shape
 
@@ -10,6 +10,13 @@ class Field < ApplicationRecord
   private
 
   def calculate_area
-    # self.area = shape.geometry.area
+    spherical_factory = RGeo::Geographic.spherical_factory(srid: 4326)
+    utm_factory = RGeo::Geographic.projected_factory(
+      projection_proj4: '+proj=utm +zone=36 +datum=WGS84 +units=m +no_defs',
+      projection_srid: 32636,
+      geographic_srid: 4326
+    )
+    projected_multipolygon = RGeo::Feature.cast(shape, factory: utm_factory)
+    self.area = projected_multipolygon.area
   end
 end
